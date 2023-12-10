adc6e479262e5d363b341ae85e6a075541b321a6
Marcus Blackhall <marcusinamsterdam@gmail.com>
2023-12-10 19:48:36 +0100

    Add basic authentication security

--- src/main/java/nl/ilovecoding/userservice/UserserviceApplication.java
+++ src/main/java/nl/ilovecoding/userservice/UserserviceApplication.java
@@ -2,8 +2,10 @@ package nl.ilovecoding.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
