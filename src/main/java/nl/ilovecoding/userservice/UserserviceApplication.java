3ee36ab069c102929edfa49fd3d9fa687c0bea19
Marcus <marcus.blackhall@splendiddata.com>
2023-12-11 11:52:32 +0100

    Add Simple Websecurity to rest requests

--- src/main/java/nl/ilovecoding/userservice/UserserviceApplication.java
+++ src/main/java/nl/ilovecoding/userservice/UserserviceApplication.java
@@ -2,14 +2,12 @@ package nl.ilovecoding.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-
 public class UserserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceApplication.class, args);
+    }
 
 }
---
+++ src/main/java/nl/ilovecoding/userservice/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package nl.ilovecoding.userservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private AppBasicAuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .csrf()
+                .disable()
+        ;
+        return http.build();
+    }
+
+
+}
